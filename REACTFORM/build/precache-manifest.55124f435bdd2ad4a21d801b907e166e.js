5e6c2ba6ce69a87840ef6501c946b5f343b45f96
coskun sahin <34971054+coskunsahin@users.noreply.github.com>
2020-04-29 23:39:17 +0300

    Add files via upload

---
+++ REACTFORM/build/precache-manifest.55124f435bdd2ad4a21d801b907e166e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "834ba70083c3e86bc4d0bd13c31004b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9144da8ca54f42e29eeb",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f3fc30218bbffbb889d",
+    "url": "/static/js/2.cb823df6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cb823df6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9144da8ca54f42e29eeb",
+    "url": "/static/js/main.443c951a.chunk.js"
+  },
+  {
+    "revision": "ce47d40c2ec9fdac1d99",
+    "url": "/static/js/runtime-main.fa7d8c7f.js"
+  }
+]);
---
+++ REACTFORM/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import CostTable  from './CostTable'
+import Masterform from './Masterforms'; 
+
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Masterform /> 
+      <CostTable />
+    </div>
+    
+  );
+}
+
+export default App;
